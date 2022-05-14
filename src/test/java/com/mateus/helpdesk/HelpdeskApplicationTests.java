b6b73716d3cc3c910cdda7757b66e91d7e2fcbfc
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-13 22:32:49 -0300

    Projeto Criado

---
+++ src/test/java/com/mateus/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
