f75bedd66771c1da20569cb5c792217731d683c1
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-14 18:33:44 -0300

    Criação do perfil de teste

--- src/main/java/com/mateus/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/mateus/helpdesk/HelpdeskApplication.java
@@ -1,52 +1,13 @@
 package com.mateus.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mateus.helpdesk.domain.Chamado;
-import com.mateus.helpdesk.domain.Cliente;
-import com.mateus.helpdesk.domain.Tecnico;
-import com.mateus.helpdesk.domain.enums.Perfil;
-import com.mateus.helpdesk.domain.enums.Prioridade;
-import com.mateus.helpdesk.domain.enums.Status;
-import com.mateus.helpdesk.repositories.ChamadoRepository;
-import com.mateus.helpdesk.repositories.ClienteRepository;
-import com.mateus.helpdesk.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner{
-	
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
+public class HelpdeskApplication {
 
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private ChamadoRepository chamadoRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "51449499082", "valdir@mail.com", "123");
-		tec1.addPerfil(Perfil.ADMIN);
-		
-		Cliente cli1 = new Cliente(null, "Linus Torvalds", "28076829011", "torvalds@mail.com", "123");
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
-		
-		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
 }
---
+++ src/main/java/com/mateus/helpdesk/services/DBService.java
@@ -0,0 +1,46 @@
+package com.mateus.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateus.helpdesk.domain.Chamado;
+import com.mateus.helpdesk.domain.Cliente;
+import com.mateus.helpdesk.domain.Tecnico;
+import com.mateus.helpdesk.domain.enums.Perfil;
+import com.mateus.helpdesk.domain.enums.Prioridade;
+import com.mateus.helpdesk.domain.enums.Status;
+import com.mateus.helpdesk.repositories.ChamadoRepository;
+import com.mateus.helpdesk.repositories.ClienteRepository;
+import com.mateus.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public void instanciaDB() {
+		
+		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "51449499082", "valdir@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "28076829011", "torvalds@mail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+		
+	}
+
+}
