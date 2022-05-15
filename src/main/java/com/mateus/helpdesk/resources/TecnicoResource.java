17ceff4f44eb16d714cf93926c1b3f186d023d40
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-14 21:25:37 -0300

    Padrão DTO para Tecnico

--- src/main/java/com/mateus/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/mateus/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mateus.helpdesk.domain.Tecnico;
+import com.mateus.helpdesk.domain.dtos.TecnicoDTO;
 import com.mateus.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,10 +19,10 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
 }
