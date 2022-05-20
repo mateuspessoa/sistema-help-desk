18d8f07803d3512b3bb954cf0bf09d55f00489fb
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-05-19 23:34:22 -0300

    Remoção e atualização de Tecnico

--- src/main/java/com/mateus/helpdesk/services/TecnicoService.java
+++ src/main/java/com/mateus/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.mateus.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +40,25 @@ public class TecnicoService {
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+		
+	}
+	
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Tecnico possui ordens de servicço e não pode ser deletado!");
+		}
+		
+		repository.deleteById(id);
+	}
+	
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		
